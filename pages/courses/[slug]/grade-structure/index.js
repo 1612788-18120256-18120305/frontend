37258793bf840514f4b33632cac90b0bac8efab0
Le Quoc Dat <18120305@student.hcmus.edu.vn>
2021-11-29 20:26:52 +0700

    create grade

--- pages/courses/[slug]/grade-structure/index.js
+++ pages/courses/[slug]/grade-structure/index.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BACKEND_URL } from '../../../../lib/Utils';
 import { getSession } from 'next-auth/react';
 import { DragDropContext, Droppable, Draggable } from '../../../../components/common/dnd_component';
+import axios from 'axios';
 
-function GradeStructure({ listAssignment }) {
+function GradeStructure({ listAssignment, _session, slug }) {
+  const [title, setTitle] = useState('');
+  const [detail, setDetail] = useState('');
+  const [assignments, setAssignments] = useState(listAssignment);
+
+  function handleChange(e) {
+    const name = e.target.name;
+    if (name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setDetail(e.target.value);
+    }
+  }
+
+  console.log(assignments);
+
+  async function handleCreateAssigment(event) {
+    event.preventDefault();
+
+    const res = await axios.post(
+      BACKEND_URL + `/courses/${slug}/assignment`,
+      { name: title, point: detail },
+      {
+        headers: {
+          Authorization: `Bearer ${_session?.jwt}`,
+        },
+      }
+    );
+
+    // const data = await res.json();
+    setAssignments([...assignments, res.data.assignment]);
+    setTitle('');
+    setDetail('');
+  }
+
   return (
-    <div className="">
+    <div className="mt-4 w-2/5 mx-auto">
       <div className="font-semibold text-center py-6 bordered">
         <h2 className="text-3xl text-red-500">Grade Structure</h2>
       </div>
       <DragDropContext>
         <Droppable droppableId="droppable">
           {(provided) => (
-            <ul className="mt-4 w-2/5 mx-auto" {...provided.droppableProps} ref={provided.innerRef}>
-              {listAssignment.map((item, index) => (
+            <ul className="" {...provided.droppableProps} ref={provided.innerRef}>
+              {assignments.map((item, index) => (
                 <Draggable key={item._id} draggableId={item._id} index={index}>
                   {(provided, snapshot) => (
                     <li
-                      className="relative flex flex-col mb-3 rounded bg-gray-100 p-4"
+                      className="flex flex-col mb-3 rounded bg-gray-100 p-4"
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
                       ref={provided.innerRef}
@@ -40,15 +75,15 @@ function GradeStructure({ listAssignment }) {
                         <button className="p-1 ml-2 bg-blue-500 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300 mb-2">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 w-6"
+                            className="h-6 w-6"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                             />
                           </svg>
@@ -72,15 +107,15 @@ function GradeStructure({ listAssignment }) {
                         <button className="p-1 ml-2 bg-red-500 text-gray-100 text-lg rounded-lg focus:border-4 border-red-300">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 w-6"
+                            className="h-6 w-6"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
                             />
                           </svg>
@@ -94,6 +129,50 @@ function GradeStructure({ listAssignment }) {
           )}
         </Droppable>
       </DragDropContext>
+      <div className="flex flex-col mt-5 mb-5 rounded bg-gray-100 p-4">
+        <h2 className="text-xl mb-2 font-semibold ">Form creator</h2>
+        <form onSubmit={handleCreateAssigment}>
+          <div className="form-group">
+            <label htmlFor="title" className="mb-2">
+              Grade title
+            </label>
+            <input
+              id="title"
+              name="title"
+              className="mb-2
+                text-sm
+                placeholder-gray-500
+                px-4
+                border border-gray-400
+                w-full
+                py-2
+                focus:outline-none focus:border-blue-400"
+              value={title}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="detail" className="mb-2">
+              Grade detail
+            </label>
+            <input
+              id="detail"
+              name="detail"
+              className="mb-2
+              text-sm
+              placeholder-gray-500
+              px-4
+              border border-gray-400
+              w-full
+              py-2
+              focus:outline-none focus:border-blue-400"
+              value={detail}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="mt-2 btn btn-primary">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
@@ -113,6 +192,8 @@ export const getServerSideProps = async (context) => {
   return {
     props: {
       listAssignment: data.assignments,
+      _session: _session,
+      slug: context.query.slug,
     },
   };
 };
