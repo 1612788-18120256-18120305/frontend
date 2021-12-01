f133cf499a5ce5e006d79f27a16799f5647db5f9
NAT <ntuan.2502@gmail.com>
2021-12-01 09:15:40 +0700

    disable grade structure in student role

--- components/Course/BC.js
+++ components/Course/BC.js
@@ -1,22 +1,20 @@
-import Link from "next/link";
+import Link from 'next/link';
 export default function BC({ _data, active }) {
   return (
     <div className="btn-group">
       <Link href={`/courses/${_data.course.slug}`}>
-        <button className={`btn ${active == "/" && "btn-active"}`}>
-          Overview
-        </button>
+        <button className={`btn ${active == '/' && 'btn-active'}`}>Overview</button>
       </Link>
       <Link href={`/courses/${_data.course.slug}/users`}>
-        <button className={`btn ${active == "users" && "btn-active"}`}>
-          Users
-        </button>
+        <button className={`btn ${active == 'users' && 'btn-active'}`}>Users</button>
       </Link>
-      <Link href={`/courses/${_data.course.slug}/grade-structure`}>
-        <button className={`btn ${active == "grade-structure" && "btn-active"}`}>
-          Grade Structure
-        </button>
-      </Link>
+      {_data.course.joinId && (
+        <Link href={`/courses/${_data.course.slug}/grade-structure`}>
+          <button className={`btn ${active == 'grade-structure' && 'btn-active'}`}>
+            Grade Structure
+          </button>
+        </Link>
+      )}
     </div>
   );
 }
