a0c607c713d89d79cc5523000a7d604357263eff
Le Quoc Dat <18120305@student.hcmus.edu.vn>
2021-12-15 19:12:59 +0700

    Design file input and create alert component

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -2,28 +2,20 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 
-function Modal({ show, onClose, children }) {
+function Modal({ show, onClose, children, actions }) {
   const [isBrowser, setIsBrowser] = useState(false);
   useEffect(() => {
     setIsBrowser(true);
   }, []);
-  console.log(show);
   const handleClose = () => {
     onClose();
   };
 
   const modalContent = show ? (
-    <div className={styles.modal_overlay_custom}>
-      <div className={styles.modal_box}>
+    <div className={styles.modal_overlay_custom} onClick={onClose}>
+      <div className={styles.modal_box} onClick={(e) => e.stopPropagation()}>
         {children}
-        <div className="modal-action">
-          <a href="#" className="btn btn-primary">
-            Import
-          </a>
-          <a href="#" className="btn" onClick={handleClose}>
-            Close
-          </a>
-        </div>
+        <div className="modal-action">{actions}</div>
       </div>
     </div>
   ) : null;
--- pages/courses/[slug]/users.js
+++ pages/courses/[slug]/users.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import BC from '../../../components/Course/BC';
 import Papa from 'papaparse';
 import Modal from '../../../components/Modal/Modal';
+import Alert from '../../../components/Alert/Alert';
 
 export default function Users({ _session, _data }) {
   const [isTeacher, setIsTeacher] = useState(
@@ -19,6 +20,8 @@ export default function Users({ _session, _data }) {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isFilePicked, setIsFilePicked] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState({});
 
   async function handleInviteTeacherSubmit() {
     const invitation = await axios.post(
@@ -121,13 +124,16 @@ export default function Users({ _session, _data }) {
 
   function handleCSVFileSubmit(event) {
     event.preventDefault();
+    if (!selectedFile) {
+      return;
+    }
+    setLoading(true);
     Papa.parse(selectedFile, {
       complete: async (result) => {
         const data = result.data;
         const studentIds = data.map((student) => {
           return student.StudentId.toString();
         });
-        console.log(studentIds);
         const res = await axios.post(
           `${BACKEND_URL}/courses/${_data.course.slug}/assignment/studentid`,
           {
@@ -140,10 +146,31 @@ export default function Users({ _session, _data }) {
           }
         );
         console.log(res.data);
+        setLoading(false);
+        setShowModal(false);
+        setAlert({ show: true, type: 'alert-success', message: 'Upload successfully!' });
+        setTimeout(() => {
+          setAlert({});
+        }, 3000);
       },
       header: true,
     });
   }
+
+  const modalActions = (
+    <>
+      <button
+        type="submit"
+        form="uploadCsvForm"
+        className={`btn btn-primary ${loading ? 'loading' : ''}`}
+      >
+        Upload
+      </button>
+      <a href="#" className="btn btn-outline btn-secondary" onClick={() => setShowModal(false)}>
+        Close
+      </a>
+    </>
+  );
 
   return (
     <>
@@ -227,22 +254,20 @@ export default function Users({ _session, _data }) {
                     )}
                   </div>
                 </div>
-                <div className="flex justify-center mt-4">
-                  <a
-                    href={'/list_student_template.csv'}
-                    className="btn btn-primary mr-4"
-                    download="Template"
-                  >
-                    Download CSV
-                  </a>
-                  <form onSubmit={handleCSVFileSubmit}>
-                    <input type="file" onChange={changeHandler} />
-                    <button className="btn">Submit</button>
-                  </form>
-                  <button className="btn btn-primary" onClick={() => setShowModal(true)}>
-                    Open modal
-                  </button>
-                </div>
+                {isTeacher && (
+                  <div className="flex justify-center mt-4">
+                    <a
+                      href={'/list_student_template.csv'}
+                      className="btn btn-primary mr-4"
+                      download="Template"
+                    >
+                      Download CSV
+                    </a>
+                    <button className="btn btn-secondary" onClick={() => setShowModal(true)}>
+                      Upload CSV
+                    </button>
+                  </div>
+                )}
                 {_data.course.students.map((student, key) => (
                   <div className="p-2 flex items-center" key={key}>
                     <img
@@ -260,11 +285,50 @@ export default function Users({ _session, _data }) {
           </div>
         </>
       )}
-      <Modal show={showModal} onClose={() => setShowModal(false)}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus nemo!
-        Distinctio deserunt assumenda possimus odio officia commodi, ad iusto doloremque. Commodi
-        aut veritatis quos harum velit obcaecati fuga pariatur!
+      <Modal show={showModal} onClose={() => setShowModal(false)} actions={modalActions}>
+        <form id="uploadCsvForm" onSubmit={handleCSVFileSubmit}>
+          <div className="flex justify-center mt-8">
+            <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
+              <div className="m-4">
+                <label className="inline-block mb-2 text-gray-500">Please upload file as csv</label>
+                <div className="flex items-center justify-center flex-col w-full">
+                  <label className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                    <div className="flex flex-col items-center justify-center pt-7">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                        />
+                      </svg>
+                      <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                        Attach a file
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      className="opacity-0"
+                      onChange={changeHandler}
+                      accept=".csv"
+                    />
+                  </label>
+                  <div>
+                    {selectedFile && <p className="textarea-info mt-4">{selectedFile.name}</p>}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </Modal>
+      <Alert alert={alert} />
     </>
   );
 }
