f162992eb578d6f91585f7f6e6f854c93d154dfa
Le Quoc Dat <18120305@student.hcmus.edu.vn>
2021-12-21 22:37:39 +0700

    mark sigle grade as finalized

--- components/Grade/InputGradeBoard.js
+++ components/Grade/InputGradeBoard.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 import { BACKEND_URL } from '../../lib/Utils';
 
 export default function InputGradeBoard({ courseSlug, assignment, item, _session }) {
-  const [grade, setGrade] = useState(assignment.grades.find((obj) => obj.id === item)?.grade);
+  const oldGrade = assignment.grades.find((obj) => obj.id === item);
+  const [draft, setDraft] = useState(oldGrade.draft);
+  const [grade, setGrade] = useState(oldGrade.grade);
+
   async function handleGradeChange(e) {
     const value = e.target.value;
     if (value > 100 || value < 0 || value.length > 3) {
       return;
     }
     setGrade(value);
-    const res = await axios.post(
+    await axios.post(
       BACKEND_URL + `/courses/${courseSlug}/assignment/${assignment._id}/grade`,
       { studentId: item, grade: value },
       {
@@ -19,12 +22,52 @@ export default function InputGradeBoard({ courseSlug, assignment, item, _session
         },
       }
     );
-    console.log(res.data);
+  }
+
+  async function handleFinalizedGrade() {
+    const res = await axios.post(
+      BACKEND_URL + `/courses/${courseSlug}/assignment/${assignment._id}/finalize`,
+      { studentId: item },
+      {
+        headers: {
+          Authorization: `Bearer ${_session?.jwt}`,
+        },
+      }
+    );
+    if (res.status === 200) {
+      setDraft(false);
+    }
   }
 
   return (
     <>
-      <input type="number" min="0" max="100" value={grade} onChange={handleGradeChange} />
+      <div className="dropdown dropdown-left absolute top-0 right-0">
+        <button
+          tabIndex="0"
+          className="finalgrade-btn bg-white rounded-full p-1 hover:bg-gray-50 active:bg-gray-150"
+        >
+          <svg focusable="false" width="24" height="24" viewBox="0 0 24 24" className="NMm5M">
+            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+          </svg>
+        </button>
+        <ul
+          tabIndex="0"
+          className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52 z-50"
+        >
+          <li className="z-50">
+            <a onClick={handleFinalizedGrade}>Mark finalized</a>
+          </li>
+        </ul>
+      </div>
+      <input
+        className={`${draft ? '' : 'text-green-600 font-bold'}`}
+        type="number"
+        min="0"
+        max="100"
+        value={grade}
+        placeholder="..."
+        onChange={handleGradeChange}
+      />
     </>
   );
 }
--- pages/courses/[slug]/grade-board/index.js
+++ pages/courses/[slug]/grade-board/index.js
@@ -66,7 +66,7 @@ export default function GradeBoard({ _session, _data }) {
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
+              <table className="min-w-full divide-y divide-gray-200 mt-4">
                 <thead className="bg-gray-50">
                   <tr>
                     <th
@@ -101,11 +101,10 @@ export default function GradeBoard({ _session, _data }) {
                     <tr key={key}>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{item}</td>
                       {assignments.map((assignment, key) => (
-                        <td key={key} className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {/* <input
-                            type="text"
-                            value={assignment.grades.find((obj) => obj.id === item)?.grade}
-                          /> */}
+                        <td
+                          key={key}
+                          className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 relative"
+                        >
                           <InputGradeBoard
                             courseSlug={_data.course.slug}
                             assignment={assignment}
