bfcc7d73666237050cd53b2585cea3fea223d2e0
Tuan Nguyen <ntuan.2502@gmail.com>
2022-01-17 14:46:07 +0700

    add reset password and activation account by code

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -149,13 +149,18 @@ export default function Login({ csrfToken }) {
                   />
                 </div>
               </div>
-              {/* <div className="flex items-center mb-6 -mt-4">
+              <div className="flex items-center mb-6 -mt-4">
                 <div className="flex ml-auto">
-                  <div className="inline-flex text-xs font-bold text-red-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white">
-                    {alert}
-                  </div>
+                  <Link href="/auth/forgot-password">
+                    <a
+                      href="#"
+                      className="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white"
+                    >
+                      Forgot Your Password?
+                    </a>
+                  </Link>
                 </div>
-              </div> */}
+              </div>
               <div className="flex w-full">
                 <div
                   onClick={(e) => handleSubmit(e)}
